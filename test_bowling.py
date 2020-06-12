56eff1b00d85b384303362ef19ecd0a89c90c362
GastonVidart <gaston.vidart@gmail.com>
2020-06-12 18:21:17 -0300

    Todos ceros (#1) (#2)
    * test VERDE
    
    pasa el primer test
    
    * refactor y test VERDE
    
    * agregar comentarios

---
+++ test_bowling.py
@@ -0,0 +1,6 @@
+from src.bowling import play_bowling
+
+def test_all_gutter():
+    #modulo para testear la partida de todos ceros
+    result = play_bowling(current_score = 0, rolls = 20, pins = 0)
+    assert result == 0
