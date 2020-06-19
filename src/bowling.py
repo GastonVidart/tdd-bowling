438442954263ee564ab869097b09bfe7148072ff
yophex <seba_yaupe@hotmail.com>
2020-06-19 18:49:12 -0300

    test Spare VERDE

--- src/bowling.py
+++ src/bowling.py
@@ -1,3 +1,20 @@
+pinesTirados=0
 def play_bowling(current_score, rolls, pins):
     #modulo para realizar una jugada
-    return current_score + rolls * pins
+    global pinesTirados 
+    if current_score==0:
+        pinesTirados=0  
+    score=current_score
+    scoreAux=0
+    print(pinesTirados)
+    if pinesTirados==0:
+        pinesTirados=rolls*pins
+        scoreAux=pinesTirados
+    else:
+        scoreAux=pinesTirados+rolls*pins
+        if scoreAux==10:
+            scoreAux=rolls*pins+1
+        pinesTirados=0 
+    score=score+scoreAux
+    return score
+
--- test_bowling.py
+++ test_bowling.py
@@ -9,3 +9,9 @@ def test_all_ones():
     #modulo para testear partida todos 1
     result = play_bowling(current_score = 0, rolls = 20, pins = 1)
     assert result == 20
+
+def test_spare():
+    result = play_bowling(current_score = 0, rolls = 1, pins = 3)
+    result = play_bowling(current_score = result, rolls = 1, pins = 7)
+    result = play_bowling(current_score = result, rolls = 18, pins = 1)
+    assert result == 29
