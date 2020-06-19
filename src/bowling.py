23e0e35b79fa77c8472d1d054c4488cd72cccbdf
yophex <seba_yaupe@hotmail.com>
2020-06-19 20:09:12 -0300

    refactor y TEST VERDE

--- src/bowling.py
+++ src/bowling.py
@@ -1,20 +1,35 @@
-pinesTirados=0
-def play_bowling(current_score, rolls, pins):
-    #modulo para realizar una jugada
-    global pinesTirados 
-    if current_score==0:
-        pinesTirados=0  
-    score=current_score
-    scoreAux=0
-    print(pinesTirados)
-    if pinesTirados==0:
-        pinesTirados=rolls*pins
-        scoreAux=pinesTirados
-    else:
-        scoreAux=pinesTirados+rolls*pins
-        if scoreAux==10:
-            scoreAux=rolls*pins+1
-        pinesTirados=0 
-    score=score+scoreAux
-    return score
 
+
+def play_bowling(current_score, rolls, pins):
+    # modulo para realizar una jugada
+    global pinesTirados, esPrimerTiro, flagSpare
+    terminado = True
+    if current_score == 0:
+        # si es el inicio del juego, se actualizan las variables
+        pinesTirados = 0
+        esPrimerTiro = True
+        flagSpare = False
+
+    for tiro in range(rolls):
+        # para cada tiro
+        current_score += pins
+        if flagSpare:
+            # hay que sumar el bonus de media chuza
+            current_score += pins
+            flagSpare = False
+
+        if esPrimerTiro:
+            # del juego actual
+            esPrimerTiro = False
+            pinesTirados = pins
+        else:
+            # segundo tiro del juego actual
+            pinesTirados += pins
+            if pinesTirados == 10:
+                # tiro todos los pinos
+                terminado = False
+                flagSpare = True
+            pinesTirados = 0
+            esPrimerTiro = True
+
+    return current_score, terminado
--- test_bowling.py
+++ test_bowling.py
@@ -2,16 +2,19 @@ from src.bowling import play_bowling
 
 def test_all_gutter():
     #modulo para testear la partida de todos ceros
-    result = play_bowling(current_score = 0, rolls = 20, pins = 0)
+    result, terminado = play_bowling(current_score = 0, rolls = 20, pins = 0)
     assert result == 0
+    assert terminado
 
 def test_all_ones():
     #modulo para testear partida todos 1
-    result = play_bowling(current_score = 0, rolls = 20, pins = 1)
+    result, terminado = play_bowling(current_score = 0, rolls = 20, pins = 1)
     assert result == 20
-
+    assert terminado
+    
 def test_spare():
-    result = play_bowling(current_score = 0, rolls = 1, pins = 3)
-    result = play_bowling(current_score = result, rolls = 1, pins = 7)
-    result = play_bowling(current_score = result, rolls = 18, pins = 1)
-    assert result == 29
+    result, terminado = play_bowling(current_score = 0, rolls = 1, pins = 3)
+    result, terminado = play_bowling(current_score = result, rolls = 1, pins = 7)
+    result, terminado = play_bowling(current_score = result, rolls = 18, pins = 1)
+    assert result == 29 
+    assert terminado
