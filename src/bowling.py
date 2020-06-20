01234838ac9ddfbaf3f7858b7c54b9893bd810ca
GastonVidart <gaston.vidart@gmail.com>
2020-06-19 21:21:48 -0300

    Nueva Versión v0.3 (#9)
    * Todos ceros (#1)
    
    * test VERDE
    
    pasa el primer test
    
    * refactor y test VERDE
    
    * agregar comentarios
    
    * test Spare VERDE
    
    * refactor y TEST VERDE
    
    * test VERDE
    
    * refactor
    
    * test ROJO
    
    * test VERDE corrige test
    
    Co-authored-by: yophex <seba_yaupe@hotmail.com>
    Co-authored-by: Et3rnauta <guidocanevello@hotmail.com>

--- src/bowling.py
+++ src/bowling.py
@@ -1,5 +1,42 @@
 def play_bowling(current_score, rolls, pins):
-    #modulo para realizar una jugada
-    return current_score + rolls * pins
+    # modulo para realizar una jugada
+    global pinesTirados, esPrimerTiro, flagSpare, flagStrike
+    terminado = True
+    if current_score == 0:
+        # si es el inicio del juego, se actualizan las variables
+        pinesTirados = 0
+        esPrimerTiro = True
+        flagSpare = False
+        flagStrike = 0
 
-    
+    for tiro in range(rolls):
+        # para cada tiro
+        current_score += pins
+
+        if flagSpare:
+            # hay que sumar el bonus de media chuza
+            current_score += pins
+            flagSpare = False
+
+        if flagStrike != 0:
+            current_score += pins
+            flagStrike = (flagStrike + 1) % 3
+
+        if esPrimerTiro:
+            # del juego actual
+            if pins == 10:
+                flagStrike = 1
+            else:
+                esPrimerTiro = False
+                pinesTirados = pins
+        else:
+            # segundo tiro del juego actual
+            pinesTirados += pins
+            if pinesTirados == 10:
+                # tiro todos los pinos
+                terminado = False
+                flagSpare = True
+            pinesTirados = 0
+            esPrimerTiro = True
+
+    return current_score, terminado
--- test_bowling.py
+++ test_bowling.py
@@ -1,12 +1,58 @@
 from src.bowling import play_bowling
 
+
 def test_all_gutter():
-    #modulo para testear la partida de todos ceros
-    result = play_bowling(current_score = 0, rolls = 20, pins = 0)
+    # modulo para testear la partida de todos ceros
+    result, terminado = play_bowling(current_score=0, rolls=20, pins=0)
     assert result == 0
+    assert terminado
 
 
 def test_all_ones():
-    #modulo para testear partida todos 1
-    result = play_bowling(current_score = 0, rolls = 20, pins = 1)
+    # modulo para testear partida todos 1
+    result, terminado = play_bowling(current_score=0, rolls=20, pins=1)
     assert result == 20
+    assert terminado
+
+
+def test_spare():
+    result, terminado = play_bowling(current_score=0, rolls=1, pins=3)
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=7)
+    result, terminado = play_bowling(current_score=result, rolls=18, pins=1)
+    assert result == 29
+    assert terminado
+
+
+def test_strike():
+    result, terminado = play_bowling(current_score=0, rolls=1, pins=10)
+    result, terminado = play_bowling(current_score=result, rolls=18, pins=1)
+    assert result == 30
+    assert terminado
+
+
+def test_mezcla_strike_spare():
+    result, terminado = play_bowling(current_score=0, rolls=1, pins=2)
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=5)
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=7)
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=2)
+    assert result == 16
+    
+    # spare
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=4)    
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=6)        
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=7)
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=2)
+    assert result == 42
+        
+    result, terminado = play_bowling(current_score=result, rolls=6, pins=3)
+    assert result == 60    
+
+    # strike
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=10)    
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=4)    
+    result, terminado = play_bowling(current_score=result, rolls=1, pins=5)    
+    assert result == 88    
+
+    result, terminado = play_bowling(current_score=result, rolls=2, pins=4)
+    assert result == 96
+    assert terminado
