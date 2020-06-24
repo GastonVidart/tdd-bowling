a97987c2efeda4332662382a8069e3b4e48295ed
GastonVidart <gaston.vidart@gmail.com>
2020-06-23 21:33:02 -0300

    Deploy (#12)
    * Todos ceros (#1)
    
    * test VERDE
    
    pasa el primer test
    
    * refactor y test VERDE
    
    * agregar comentarios
    
    * test Spare VERDE
    
    * refactor y TEST VERDE
    
    * test VERDE
    
    * refactor
    
    * test ROJO
    
    * test VERDE corrige test
    
    * test ROJO
    
    * test VERDE rondaFinal
    
    * refactor rondaFinal
    
    * test VERDE all strike
    
    Co-authored-by: yophex <seba_yaupe@hotmail.com>
    Co-authored-by: Et3rnauta <guidocanevello@hotmail.com>
    Co-authored-by: GuidoCanevello <43616779+Et3rnauta@users.noreply.github.com>

--- src/bowling.py
+++ src/bowling.py
@@ -1,42 +1,62 @@
 def play_bowling(current_score, rolls, pins):
     # modulo para realizar una jugada
-    global pinesTirados, esPrimerTiro, flagSpare, flagStrike
+    global pinesTirados, esPrimerTiro, flagSpare, flagStrike, rondaActual, rondaFinal, strikeActivos
     terminado = True
-    if current_score == 0:
+    if current_score == -1:
         # si es el inicio del juego, se actualizan las variables
         pinesTirados = 0
         esPrimerTiro = True
+        # Es primer tiro de la ronda
         flagSpare = False
         flagStrike = 0
+        rondaActual = 1
+        rondaFinal = False
+        current_score = 0
+        strikeActivos = 0
 
     for tiro in range(rolls):
         # para cada tiro
-        current_score += pins
-
+        rondaFinal = rondaActual == 11
+        # la ronda final se computa de manera diferente a las demas
         if flagSpare:
             # hay que sumar el bonus de media chuza
             current_score += pins
             flagSpare = False
 
         if flagStrike != 0:
-            current_score += pins
+            # Si tiene bonus de chuza
+            current_score += pins * strikeActivos
+            # reinicia el flag cada 2 rondas
             flagStrike = (flagStrike + 1) % 3
+            if strikeActivos > 1:
+                strikeActivos -= 1
+            elif flagStrike == 0:
+                strikeActivos == 0
 
-        if esPrimerTiro:
-            # del juego actual
-            if pins == 10:
-                flagStrike = 1
+        if not rondaFinal:
+            # juego normal
+            current_score += pins
+            if esPrimerTiro:
+                # de la ronda actual
+                if pins == 10:
+                    flagStrike = 1
+                    rondaActual += 1
+                    strikeActivos += 1
+                    # La ronda avanza porque se hizo una chuza
+                else:
+                    esPrimerTiro = False
+                    pinesTirados = pins
             else:
-                esPrimerTiro = False
-                pinesTirados = pins
-        else:
-            # segundo tiro del juego actual
-            pinesTirados += pins
-            if pinesTirados == 10:
-                # tiro todos los pinos
-                terminado = False
-                flagSpare = True
-            pinesTirados = 0
-            esPrimerTiro = True
+                # segundo tiro de la ronda actual
+                pinesTirados += pins
+                if pinesTirados == 10:
+                    # completo media chuza
+                    terminado = False
+                    flagSpare = True
+                elif pinesTirados > 10:
+                    print("Tiro incorrecto Fin del Juego!")
+                pinesTirados = 0
+                esPrimerTiro = True
+                rondaActual += 1
 
     return current_score, terminado
